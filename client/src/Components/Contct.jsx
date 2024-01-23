965bb8245286ff668f41eddd029963016ae9bee1
Toufique Elahi Tonmoy <toufique0611@gmail.com>
2024-01-23 16:10:03 +0600

    contact page

--- client/src/Components/Contct.jsx
+++ client/src/Components/Contct.jsx
@@ -1,9 +1,23 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useRef} from 'react';
+import emailjs from '@emailjs/browser';
 
 const Contct = () => {
-  useEffect(() => {
-    window.scrollTo(0, 0);
-}, []);
+
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+    
+
+    emailjs.sendForm('service_iyikmif', 'template_swfcjuo', form.current, '2R1aN_RXxNyo3j1Ym')
+      .then((result) => {
+          console.log(result.text);
+          e.target.reset();
+      }, (error) => {
+          console.log(error.text);
+      });
+    }
+
   return (
     <div>
       <div className="hero min-h-screen ">
@@ -13,25 +27,25 @@ const Contct = () => {
             <p className="py-6 text-justify text-gray-600">We’re a super friendly bunch and here to help you out with any query you may have. No question is too small, so don’t be shy! Just get in touch using the contact form below and one of our fantastic support team will get back to you as soon as possible.</p>
           </div>
           <div className="card shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
-            <form className="card-body">
+            <form className="card-body" ref={form} onSubmit={sendEmail}>
               <div className="form-control">
                 <label className="label">
                   <span className="label-text">Name</span>
                 </label>
-                <input type="text" placeholder="Name" className="input input-bordered" required />
+                <input type="text" placeholder="Name" name="user_name" className="input input-bordered" required />
               </div>
 
               <div className="form-control">
                 <label className="label">
                   <span className="label-text">Email</span>
                 </label>
-                <input type="email" placeholder="email" className="input input-bordered" required />
+                <input type="email" placeholder="email" name="user_email" className="input input-bordered" required />
               </div>
               <div className="form-control">
                 <label className="label">
                   <span className="label-text">Message</span>
                 </label>
-                <input type="text" placeholder="Message" className="input input-bordered" required />
+                <textarea placeholder="Message" name="message" className="input input-bordered" required />
               </div>
               <div className="form-control mt-6">
                 <button className="btn btn-primary text-white">Submit</button>
